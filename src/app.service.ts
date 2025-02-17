af7623cb0cf0b274b832d2965202787daeef2714
moyui <gxy1510@gmail.com>
2025-02-17 17:58:03 +0800

    feat: binding测试

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import * as path from 'path';
 
 @Injectable()
 export class AppService {
+  async loadNativeModule() {
+    await import(path.resolve(__dirname, 'native', 'xxx.node'));
+  }
   getHello(): string {
     return 'Hello World!';
   }
