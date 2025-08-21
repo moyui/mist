9802875746e6edc6397efa873f3be3ff65985f2b
moyui <gxy1510@gmail.com>
2025-08-21 23:59:12 +0800

    feat: 结构化输出

--- apps/saya/src/role/role.module.ts
+++ apps/saya/src/role/role.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { LlmModule } from '../llm/llm.module';
 import { TemplateModule } from '../template/template.module';
 import { RoleController } from './role.controller';
 import { RoleService } from './role.service';
 
 @Module({
-  imports: [TemplateModule],
+  imports: [TemplateModule, LlmModule, ConfigModule],
   controllers: [RoleController],
   providers: [RoleService],
 })
--- apps/saya/src/role/role.service.ts
+++ apps/saya/src/role/role.service.ts
@@ -1,17 +1,29 @@
+import { AngentsConfig } from '@app/config';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { LlmService } from '../llm/llm.service';
 import { TemplateService } from '../template/template.service';
 import { State } from './dto/state.dto';
 
 @Injectable()
 export class RoleService {
-  constructor(private templateService: TemplateService) {}
+  constructor(
+    private templateService: TemplateService,
+    private llmService: LlmService,
+    private configService: ConfigService,
+  ) {}
 
-  Commander(state: State) {
+  async Commander(state: State) {
     // 1. 接收用户指令/目标
-    const messages = this.templateService.applyPromptTemplate({
+    const messages = await this.templateService.applyPromptTemplate({
       name: 'commander',
       state,
     });
+    const agentsConfig = this.configService.get<AngentsConfig>('agents');
+    const llm = this.llmService.getLLMByType(agentsConfig['Commander']);
+    const response = await llm.withStructuredOutput({}).invoke(messages);
+    const goTo = response['next'];
+
     return state;
   }
 
