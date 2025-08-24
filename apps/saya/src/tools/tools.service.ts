2718781a5d5a5227866a57f2a1809471677a757a
moyui <gxy1510@gmail.com>
2025-08-24 23:00:35 +0800

    feat: 工具函数实现

--- apps/saya/src/tools/tools.service.ts
+++ apps/saya/src/tools/tools.service.ts
@@ -1,13 +1,24 @@
-import { Injectable } from '@nestjs/common';
-import { HttpService } from '@nestjs/axios';
-import { tool } from '@langchain/core/tools';
-import { catchError, firstValueFrom } from 'rxjs';
-import { Inject, HttpException, HttpStatus, Logger } from '@nestjs/common';
-import { LocalServiceDto } from './dto/local-service.dto';
-import { AxiosError } from 'axios';
-import { KDto } from 'apps/mist/src/indicator/dto/k.dto';
-import { KVo } from 'apps/mist/src/indicator/vo/k.vo';
 import { UtilsService } from '@app/utils';
+import { tool } from '@langchain/core/tools';
+import { HttpService } from '@nestjs/axios';
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+import { KDto } from 'apps/mist/src/indicator/dto/k.dto';
+import { KDJDto } from 'apps/mist/src/indicator/dto/kdj.dto';
+import { MACDDto } from 'apps/mist/src/indicator/dto/macd.dto';
+import { RSIDto } from 'apps/mist/src/indicator/dto/rsi.dto';
+import { KVo } from 'apps/mist/src/indicator/vo/k.vo';
+import { KDJVo } from 'apps/mist/src/indicator/vo/kdj.vo';
+import { MACDVo } from 'apps/mist/src/indicator/vo/macd.vo';
+import { RSIVo } from 'apps/mist/src/indicator/vo/rsi.vo';
+import { AxiosError } from 'axios';
+import { catchError, firstValueFrom } from 'rxjs';
+import { LocalServiceDto } from './dto/local-service.dto';
 
 @Injectable()
 export class ToolsService {
@@ -52,7 +63,59 @@ export class ToolsService {
       },
       {
         name: 'get_k_tool',
-        description: '',
+        description:
+          '根据提供的 symbol、code、startDate、endDate 从本地系统获取股票的k线数据。输入必须是包含 symbol (字符串类型)、code (字符串类型)、startDate(13位数字类型)、endDate(13位数字类型) 的对象。',
+      },
+    );
+  }
+
+  createGetKDJTool() {
+    return tool(
+      async (params: KDJDto) => {
+        const kdjs = this.getLocalService<KDJDto, KDJVo[]>({
+          url: this.utilsService.getLocalUrl('kdj'),
+          params,
+        });
+        return this.utilsService.formatLocalResult(kdjs);
+      },
+      {
+        name: 'get_kdj_tool',
+        description:
+          '根据提供的 symbol、code、startDate、endDate 从本地系统获取股票的kdj数据。输入必须是包含 symbol (字符串类型)、code (字符串类型)、startDate(13位数字类型)、endDate(13位数字类型) 的对象。',
+      },
+    );
+  }
+
+  createGetMACDTool() {
+    return tool(
+      async (params: MACDDto) => {
+        const macds = this.getLocalService<MACDDto, MACDVo[]>({
+          url: this.utilsService.getLocalUrl('macd'),
+          params,
+        });
+        return this.utilsService.formatLocalResult(macds);
+      },
+      {
+        name: 'get_macd_tool',
+        description:
+          '根据提供的 symbol、code、startDate、endDate 从本地系统获取股票的macd数据。输入必须是包含 symbol (字符串类型)、code (字符串类型)、startDate(13位数字类型)、endDate(13位数字类型) 的对象。',
+      },
+    );
+  }
+
+  createGetRSITool() {
+    return tool(
+      async (params: RSIDto) => {
+        const rsis = this.getLocalService<RSIDto, RSIVo[]>({
+          url: this.utilsService.getLocalUrl('rsi'),
+          params,
+        });
+        return this.utilsService.formatLocalResult(rsis);
+      },
+      {
+        name: 'get_rsi_tool',
+        description:
+          '根据提供的 symbol、code、startDate、endDate 从本地系统获取股票的rsi数据。输入必须是包含 symbol (字符串类型)、code (字符串类型)、startDate(13位数字类型)、endDate(13位数字类型) 的对象。',
       },
     );
   }
