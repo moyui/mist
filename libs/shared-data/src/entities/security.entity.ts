9bc08e4a07ee8325d943b57c2a1376e24fb420f7
moyui <gxy1510@gmail.com>
2026-03-21 17:03:17 +0800

    feat: add Security entity

---
+++ libs/shared-data/src/entities/security.entity.ts
@@ -0,0 +1,68 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  Unique,
+} from 'typeorm';
+import { SecurityType } from '../enums/security-type.enum';
+import { SecurityStatus } from '../enums/security-status.enum';
+import { SecuritySourceConfig } from './security-source-config.entity';
+import { MarketDataBar } from './market-data-bar.entity';
+
+@Entity({ name: 'securities' })
+@Unique(['code'])
+export class Security {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 20,
+    unique: true,
+    comment: '纯代码，如 000001, 000300',
+  })
+  code: string;
+
+  @Column({
+    type: 'varchar',
+    length: 100,
+    comment: '证券名称',
+  })
+  name: string;
+
+  @Column({
+    type: 'enum',
+    enum: SecurityType,
+    comment: '证券类型：STOCK=股票，INDEX=指数',
+  })
+  type: SecurityType;
+
+  @Column({
+    type: 'varchar',
+    length: 10,
+    comment: '交易所：SH=上交所，SZ=深交所，CSI=中证指数',
+  })
+  exchange: string;
+
+  @Column({
+    type: 'tinyint',
+    default: SecurityStatus.ACTIVE,
+    comment: '状态：1=正常，0=停牌，-1=退市/终止',
+  })
+  status: SecurityStatus;
+
+  @OneToMany(() => SecuritySourceConfig, (config) => config.security)
+  sourceConfigs: SecuritySourceConfig[];
+
+  @OneToMany(() => MarketDataBar, (bar) => bar.security)
+  marketDataBars: MarketDataBar[];
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
